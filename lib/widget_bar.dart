ec04dfa0b5ae373e3709a0ebc8d971cce0f164f5
Hugo-99 <46873317+Hugo-99@users.noreply.github.com>
2022-02-13 17:13:36 +0000

    Added Map features
    Used mapbox API to display the user location. Added new dependencies for the new features.

--- lib/widget_bar.dart
+++ lib/widget_bar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:user_location/user_location.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class NavBar extends StatefulWidget {
   @override
@@ -7,10 +10,16 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  _NavBarState() {}
 
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  MapController mapController = MapController();
+  late UserLocationOptions userLocationOptions;
+  // ADD THIS
+  List<Marker> markers = [];
 
   static List<Widget> _widgetOptions = <Widget>[
     // Put your widgets in here
@@ -35,13 +44,49 @@ class _NavBarState extends State<NavBar> {
     });
   }
 
+  Widget _selectedWidget(int index, BuildContext context) {
+    if (index < 3) {
+      return _widgetOptions.elementAt(_selectedIndex);
+    } else {
+      return FlutterMap(
+        options: MapOptions(
+          center: LatLng(0, 0),
+          zoom: 15.0,
+          plugins: [
+            // ADD THIS
+            UserLocationPlugin(),
+          ],
+        ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate:
+                "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+            additionalOptions: {
+              // Paste the access token from the token.txt
+              'accessToken': '<access-token>>',
+              'id': 'mapbox/streets-v11',
+            },
+          ),
+          //MarkerLayerOptions(markers: markers),
+          userLocationOptions,
+        ],
+        mapController: mapController,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    userLocationOptions = UserLocationOptions(
+      context: context,
+      mapController: mapController,
+      markers: markers,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Center(child: const Text('CheckMate')),
       ),
-      body: _widgetOptions.elementAt(_selectedIndex),
+      body: _selectedWidget(_selectedIndex, context),
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.black,
         type: BottomNavigationBarType.fixed,
@@ -55,8 +100,12 @@ class _NavBarState extends State<NavBar> {
             label: 'Swipe',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              label: 'Chat',
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
           ),
         ],
         currentIndex: _selectedIndex,
